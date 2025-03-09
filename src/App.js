51d951ffb4a0d2249041de9748c0beb6128dfe61
Priyanka Bawanna <priyankabawanna@Priyankas-MacBook-Air.local>
2025-03-09 20:18:28 +0530

    Counter APP

--- src/App.js
+++ src/App.js
@@ -1,14 +1,56 @@
 import CountriesSearch from "./CountriesSearch;";
 import Weather from "./Weather";
 import XDictionary from "./XDictionary";
+import React, { Component } from 'react';
 import "./App.css"
 
 function App() {
   return (
   <>
-<CountriesSearch/> 
+<Counter/> 
   </>
   );
 }
 
 export default App;
+
+
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+    // Initialize the state with count set to 0
+    this.state = {
+      count: 0,
+    };
+  }
+
+  // Increment method to increase the count
+  increment = () => {
+    this.setState(prevState => ({
+      count: prevState.count + 1,
+    }));
+  };
+
+  // Decrement method to decrease the count
+  decrement = () => {
+    this.setState(prevState => ({
+      count: prevState.count - 1,
+    }));
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Counter App</h1> {/* Display the count */}
+        <div>Count: {this.state.count}</div>
+        <br/>
+        <button onClick={this.increment}>Increment</button>
+
+        {/* Decrement button */}
+        <button onClick={this.decrement}>Decrement</button>
+      </div>
+    );
+  }
+}
+
+
